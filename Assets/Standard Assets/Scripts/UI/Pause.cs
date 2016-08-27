1d230a9a38c837dcb6197780abc033392e3e443e
rub0 <rub0te@gmail.com>
2016-08-27 23:51:52 +0200

    First menus

---
+++ Assets/Standard Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject pauseMenu;
+
+	public void exitToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void continueGame()
+    {
+        Time.timeScale = 1.0f;
+        pauseMenu.SetActive(false);
+    }
+}
