bf79ef24165947fe08a817a8c717e7307fd78bae
rub0 <rub0te@gmail.com>
2016-08-27 17:15:20 +0200

    movement and physics

---
+++ Assets/Standard Assets/Scripts/General Scripts/Controller2D.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller2D : MonoBehaviour {
+
+    private Rigidbody2D _rigidbody;
+
+    private Vector2 extents;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        Vector3 ext = GetComponent<BoxCollider2D>().bounds.extents;
+        extents = new Vector2(ext.x, ext.y);
+    }
+
+    public class Flags
+    {
+        public bool above = false;
+        public bool below = false;
+        public bool sides = false;
+    }
+    
+    public Flags move(Vector2 direction, float delta)
+    {
+        Flags result = new Flags();
+
+        moveHorizontally(ref direction, ref result);
+        moveVertically(ref direction, ref result);
+
+        transform.Translate(direction * delta);
+
+        return result;
+    }
+
+    public void moveHorizontally(ref Vector2 direction, ref Flags result)
+    {
+        bool isGoingRight = direction.x > 0;
+
+        float rayDistance = Mathf.Abs(direction.x); ;
+
+        Vector2 rayDir = isGoingRight ? Vector2.right : Vector2.left;
+
+        int layerMask = 1 << LayerMask.NameToLayer("Ground");
+
+
+        //first raycast
+        Vector2 ray = new Vector2(transform.position.x + (extents.x * rayDir.x), transform.position.y + extents.y - 0.05f);
+
+        RaycastHit2D hit = Physics2D.Raycast(ray, rayDir,rayDistance , layerMask);
+
+        if (hit)
+        {
+            direction.x = hit.point.y - ray.y;
+            rayDistance = Mathf.Abs(direction.y);
+            result.sides = true;
+            if (isGoingRight)
+            {
+                direction.x += extents.x;
+            }
+            else
+            {
+                direction.x -= extents.x;
+            }
+        }
+        //second raycast
+        ray = new Vector2(transform.position.x + (extents.x * rayDir.x), transform.position.y - extents.y + 0.05f);
+
+        hit = Physics2D.Raycast(ray, rayDir, rayDistance, layerMask);
+
+        if (hit)
+        {
+            direction.y = hit.point.y - ray.y;
+            rayDistance = Mathf.Abs(direction.y);
+            result.sides = true;
+            if (isGoingRight)
+            {
+                direction.x -= extents.x;
+            }
+            else
+            {
+                direction.x += extents.x;
+            }
+        }
+    }
+
+    public void moveVertically(ref Vector2 direction, ref Flags result)
+    {
+        bool isGoingUp = direction.y > 0;
+
+        float rayDistance = Mathf.Abs(direction.y);
+
+        Vector2 rayDir = isGoingUp ? Vector2.up : Vector2.down;
+
+        int layerMask = 1 << LayerMask.NameToLayer("Ground");
+
+
+        //first raycast
+        Vector2 ray = new Vector2(transform.position.x + extents.x - 0.05f, transform.position.y + (extents.y * rayDir.y));
+
+        RaycastHit2D hit = Physics2D.Raycast(ray, rayDir, rayDistance * Time.fixedDeltaTime, layerMask);
+
+        if (hit)
+        {
+            direction.y = hit.point.y - ray.y;
+            rayDistance = Mathf.Abs(direction.y);
+
+            // remember to remove the skinWidth from our deltaMovement
+            if (isGoingUp)
+            {
+                result.above = true;
+            }
+            else
+            {
+                result.below = true;
+            }
+        }
+        //second raycast
+        ray = new Vector2(transform.position.x - extents.x + 0.05f, transform.position.y + (extents.y * rayDir.y));
+
+        hit = Physics2D.Raycast(ray, rayDir, rayDistance * Time.fixedDeltaTime, layerMask);
+
+        if (hit)
+        {
+            direction.y = hit.point.y - ray.y;
+            rayDistance = Mathf.Abs(direction.y);
+
+            // remember to remove the skinWidth from our deltaMovement
+            if (isGoingUp)
+            {
+                result.above = true;
+            }
+            else
+            {
+                result.below = true;
+            }
+        }
+    }
+}
---
+++ Assets/Standard Assets/Scripts/General Scripts/MovementController.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour
+{
+
+    // Ideally the component that controls the input sets
+    // the direction on this component or the ai
+    public InputManager input;
+
+    // Velocity/Acceleration values
+    public float _maxVelocity = 1.0f;
+    public float _acceleration = 2f;
+    public float _jumpForce = 5.0f;
+    private float _velFriction;
+
+    public float _gravity = 1.0f;
+
+    // I assume for testing purposes
+    public bool jumpAllowed = true;
+
+    // Needs to be public?
+    public Vector2 _velocity;
+
+    // Collisions
+    private bool _isTouchingGround = false;
+    private bool _isTouchingCeiling = false;
+
+    // Jump
+    public float _jumpSpeed = 8.4f;
+    public float _jumpMaxTime = 0.8f;
+    private bool _canJump = false;
+    private bool _jumpPressed = false;
+    private Vector3 _externalForce = Vector3.zero;
+    public float _externalForceTime = 0.2f;
+
+    private Transform _transform;
+
+    public Controller2D controller;
+
+    Vector3 inputmovement = Vector3.zero;
+
+    [HideInInspector]
+    public float _componentInitTimeStamp = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        InputManager.Instance.registerAxis("Horizontal", OnInputXAxis);
+        InputManager.Instance.RegisterKeyDown("jump", OnJumpPressed);
+        // @todo
+        // Assign manager by callback. Note that the manager
+        // has to implement IMovementCommands interface for this
+        // to work as expected.
+        input = GetComponent<InputManager>();
+        _transform = transform;
+        _velocity = Vector3.zero;
+        _isTouchingGround = true;
+
+        controller = GetComponent<Controller2D>();
+
+        _velFriction = _maxVelocity / (_maxVelocity + (0.5f * _acceleration * Time.fixedDeltaTime * 100));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (inputmovement != Vector3.zero)
+        {
+            float horizontalMomentum = inputmovement.x * _acceleration;
+            _velocity.x += horizontalMomentum;
+        }
+
+        //if needed, perform jump
+        jump();
+
+        _velocity.x *= _velFriction * ((_isTouchingGround) ? Mathf.Abs(inputmovement.x) : 1);
+        
+        if (_velocity.sqrMagnitude < float.Epsilon)
+        {
+            _velocity = Vector3.zero;
+        }
+
+        _velocity.y -= _gravity * Time.fixedDeltaTime;
+
+
+        // Vertical cap
+        if (_velocity.y > 14.0f)
+            _velocity.y = 14.0f;
+        if (_velocity.y < -14.0f)
+            _velocity.y = -14.0f;
+
+        Vector2 oldPos = _transform.position;
+        
+        Controller2D.Flags flags = controller.move(_velocity, Time.deltaTime);
+
+        if(flags.below)
+        {
+            _velocity.y = 0;
+            _isTouchingGround = true;
+        }
+        else
+        {
+            _isTouchingGround = false;
+        }
+
+        if (flags.sides)
+        {
+            _velocity.x = 0;
+        }
+        if(flags.above)
+        {
+            _velocity.y = 0;
+            _isTouchingCeiling = true;
+        }
+        else
+        {
+            _isTouchingCeiling = false;
+        }
+
+        Vector2 newPos = transform.position;
+        if (oldPos == newPos)
+            _velocity = Vector3.zero;
+    }
+
+    void jump()
+    {
+        //comprobamos si tiene una tile encima
+        if (_canJump && _isTouchingGround && !_isTouchingCeiling)
+        {
+            _canJump = false;
+            _velocity.y += _jumpForce;
+            _isTouchingGround = false;
+        }
+        
+
+    }
+
+    public Vector3 getVelocity()
+    {
+        return _velocity;
+    }
+
+    public void AddExternalForce(Vector2 force, float maxForce = float.MaxValue)
+    {
+        if (!_isTouchingGround)
+            _velocity.y = 0.0f;
+
+        _velocity += force;
+
+        // Should cap the force rather than the velocity
+        if (_velocity.y > maxForce)
+            _velocity.y = maxForce;
+
+        _isTouchingGround = false;
+    }
+
+    public void AddExternalAcceleration(Vector2 vAcceleration)
+    {
+        _velocity += vAcceleration * Time.deltaTime;
+    }
+
+    public void OnInputXAxis(string axe, float value)
+    {
+        inputmovement.x = value;
+    }
+
+    public void OnJumpPressed(string key)
+    {
+        if(_isTouchingGround)
+            _canJump = true;
+    }
+}
