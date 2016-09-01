cc57ec617253b40da27462198196b84126b7214e
Thenanox <thenanox@gmail.com>
2016-09-01 22:51:04 +0200

    refactor(weapon): DEFINITIVO

--- Assets/Scripts/Level/Weapon.cs
+++ Assets/Scripts/Level/Weapon.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System;
 
 public class Weapon : MonoBehaviour {
 
@@ -34,92 +35,38 @@ public class Weapon : MonoBehaviour {
     void TransformTile()
     {
         Collider2D hit = Physics2D.OverlapBox(new Vector2(cursor.transform.position.x, cursor.transform.position.y), new Vector2(0.5f, 0.5f), 0.0f, LayerMask.GetMask("Foreground", "Background"));
-        if (hit)
+        Collider2D hitbox = Physics2D.OverlapBox(new Vector2(cursor.transform.position.x, cursor.transform.position.y), new Vector2(0.5f, 0.5f), 0.0f, LayerMask.GetMask("Box", "Players"));
+        TransformableTile transTile = hit.transform.gameObject.GetComponent<TransformableTile>();
+        if (hit && hitbox == null && transTile != null)
         {
-            Collider2D hitbox = Physics2D.OverlapBox(new Vector2(cursor.transform.position.x, cursor.transform.position.y), new Vector2(0.5f, 0.5f), 0.0f, LayerMask.GetMask("Box", "Players"));
-            if (lastTile == null )
+            if (lastTile == null)
             {
-                lastTile = hit.transform.gameObject.GetComponent<TransformableTile>();
-                if(lastTile)
-                {
-                    lastTile.transformState();
-                }
-                else
-                {
-                    audiosource.PlayOneShot(wrongSound);
-                }  
+                lastTile = transTile;
+                lastTile.transformState();
             }
-            else if (hit.gameObject.layer == LayerMask.NameToLayer("Foreground") && lastTile.gameObject.layer == LayerMask.NameToLayer("Foreground"))
+            else if (lastTile == transTile)
             {
-                if (lastTile != hit.transform.gameObject.GetComponent<TransformableTile>())
-                {
-                    lastTile.originalState();
-                    lastTile = hit.transform.gameObject.GetComponent<TransformableTile>();
-                    lastTile.transformState();
-                }
-                else
-                {
-                    lastTile.originalState();
-                    lastTile = null;
-                }
+                lastTile.originalState();
+                lastTile = null;
             }
-            else if (hit.gameObject.layer == LayerMask.NameToLayer("Background") && !hitbox)
-            {
-                if (lastTile == hit.transform.gameObject.GetComponent<TransformableTile>())
-                {
-                    lastTile.originalState();
-                    lastTile = null;
-
-                }
-                else if (hit.transform.gameObject.GetComponent<TransformableTile>() == null)
-                {
-                    lastTile.originalState();
-                    lastTile = null;
-
-                }
-                else if (lastTile.gameObject.layer == LayerMask.NameToLayer("Background"))
-                {
-                    Collider2D hitLastileBox = Physics2D.OverlapBox(new Vector2(lastTile.transform.position.x, lastTile.transform.position.y), new Vector2(0.5f, 0.5f), 0.0f, LayerMask.GetMask("Box", "Players"));
-                    if (!hitLastileBox)
-                    {
-                        lastTile.originalState();
-                        lastTile = hit.transform.gameObject.GetComponent<TransformableTile>();
-                        if (lastTile)
-                        {
-                            lastTile.transformState();
-                        }
-                        else
-                        {
-                            audiosource.PlayOneShot(wrongSound);
-                        }
-                    } else
-                    {
-                        audiosource.PlayOneShot(wrongSound);
-                    }
-                }
-                else
-                {
-                    lastTile.originalState();
-                    lastTile = hit.transform.gameObject.GetComponent<TransformableTile>();
-                    lastTile.transformState();
-                }
-            }
-            else if (lastTile.gameObject.layer == LayerMask.NameToLayer("Background"))
+            else
             {
                 Collider2D hitLastileBox = Physics2D.OverlapBox(new Vector2(lastTile.transform.position.x, lastTile.transform.position.y), new Vector2(0.5f, 0.5f), 0.0f, LayerMask.GetMask("Box", "Players"));
-                if (!hitLastileBox)
+                if (hitLastileBox == null)
                 {
                     lastTile.originalState();
-                    lastTile = hit.transform.gameObject.GetComponent<TransformableTile>();
+                    lastTile = transTile;
                     lastTile.transformState();
-                } else
+                }
+                else
                 {
                     audiosource.PlayOneShot(wrongSound);
                 }
-            } else
-            {
-                audiosource.PlayOneShot(wrongSound);
-            }          
+            }
+        }
+        else
+        {
+            audiosource.PlayOneShot(wrongSound);
         }
     }
 
